6e5b0d20157e3c3f219991796b078f932646a820
Megan <meganmoulos@gmail.com>
2022-08-17 12:00:30 -0700

    adding form functionality

--- src/index.js
+++ src/index.js
@@ -8,15 +8,21 @@ const ramenMenu = document.querySelector('#ramen-menu')
 const ramenDetails = document.querySelector('#ramen-detail')
 const ramenDetailImage = document.querySelector('.detail-image')
 const ramenName = document.querySelector('.name')
+const ramenRestaurant = document.querySelector('.restaurant')
+const ratingDisplay = document.querySelector('#rating-display')
+const commentDisplay = document.querySelector('#comment-display')
+const form = document.querySelector('#new-ramen')
 
 // Event Listeners
+form.addEventListener('submit', getFormInfo)
 
 // Fetchers
 function getAllRamen(url) {
     return fetch(url)
     .then(res => res.json())
     .then(data => {
-        renderTopMenu(data)
+        renderARamen(data[0])
+        renderTopMenu(data) 
     })
 }
 
@@ -25,18 +31,34 @@ function renderMenuItem(item){
     const ramenMenuImage = document.createElement('img')
     ramenMenuImage.src = item.image 
     ramenMenu.appendChild(ramenMenuImage)
+    ramenMenuImage.addEventListener('click', () => renderARamen(item))
 }
 
 function renderTopMenu(data){
     data.forEach(data => renderMenuItem(data))
 }
 
+function renderARamen(item){
+    ramenDetailImage.src = item.image
+    ramenName.textContent = item.name
+    ramenRestaurant.textContent = item.restaurant
+    ratingDisplay.textContent = item.rating
+    commentDisplay.textContent = item.comment
+}
+
+function getFormInfo(e){
+    e.preventDefault()
+    console.log(e.target)
+    let name = e.target['new-name'].value
+    let restaurant = e.target['new-restaurant'].value
+    let image = e.target['new-image'].value
+    // let rating = e.target
+    let comment = e.target.['new-comment'].value
+}
+
+// Call Functions
 getAllRamen(url)
 
-
-// Click on an image from the #ramen-menu div and see all the info about that
-// ramen displayed inside the #ramen-detail div and where it says
-// insert comment here and insert rating here.
 
 // Create a new ramen after submitting the new-ramen form. The new ramen should
 // be added to the#ramen-menu div. The new ramen does not need to persist; in
